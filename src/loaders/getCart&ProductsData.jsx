d24a1d910ed5b2957d30221a94f2eacfceab62f5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-09 10:44:33 +0600

    a

---
+++ src/loaders/getCart&ProductsData.jsx
@@ -0,0 +1,16 @@
+import { getStoredCart } from "../utils/fakeDB";
+export const productsAddCartData = async () => {
+  const productsData = await fetch("products.json");
+  const products = await productsData.json();
+  const savedCart = getStoredCart();
+  const initialCart = [];
+  for (const id in savedCart) {
+    const foundProduct = products.find((product) => product.id === id);
+    if (foundProduct) {
+      const quantity = savedCart[id];
+      foundProduct.quantity = quantity;
+      initialCart.push(foundProduct);
+    }
+  }
+  return { products, initialCart };
+};
--- src/utils/router.js
+++ src/utils/router.js
@@ -5,12 +5,14 @@ import Shop from "../components/Shop";
 import Cart from "../components/Cart";
 import About from "../components/About";
 import Main from "../components/Main";
+import { productsAddCartData } from "../loaders/getCart&ProductsData";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
     errorElement: <ErrorPage></ErrorPage>,
+    loader: productsAddCartData,
     children: [
       { path: "/", element: <Home></Home> },
       { path: "/shop", element: <Shop></Shop> },
