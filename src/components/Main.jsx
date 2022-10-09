04a6857638d3bdf5730112248f1286a07b0006d6
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-09 10:33:43 +0600

    adfa

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
 function Main() {
   return (
     <div>
       <Header></Header>
-
+      <Outlet></Outlet>
       <Footer></Footer>
     </div>
   );
--- src/utils/router.js
+++ src/utils/router.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../components/ErrorPage";
 import Home from "../components/Home";
+import Shop from "../components/Shop";
+import Cart from "../components/Cart";
+import About from "../components/About";
 import Main from "../components/Main";
 
 const router = createBrowserRouter([
@@ -10,9 +13,9 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage></ErrorPage>,
     children: [
       { path: "/", element: <Home></Home> },
-      { path: "/shop", element: <Home></Home> },
-      { path: "/cart", element: <Home></Home> },
-      { path: "/about", element: <Home></Home> },
+      { path: "/shop", element: <Shop></Shop> },
+      { path: "/cart", element: <Cart></Cart> },
+      { path: "/about", element: <About></About> },
     ],
   },
 ]);
