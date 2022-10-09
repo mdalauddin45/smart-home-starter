e936fffd40ce53610b11e132fa93092297f2adb0
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-09 10:52:52 +0600

    dfsd

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { createContext } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
 
+export const ProductContext = createContext([]);
 function Main() {
+  const { products, initialCart } = useLoaderData();
   return (
     <div>
-      <Header></Header>
-      <Outlet></Outlet>
-      <Footer></Footer>
+      <ProductContext.Provider value={products}>
+        <Header></Header>
+        <Outlet></Outlet>
+        <Footer></Footer>
+      </ProductContext.Provider>
     </div>
   );
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ProductContext } from "./Main";
+import Product from "./Product";
 
 const Shop = () => {
+  const products = useContext(ProductContext);
+  console.log(products);
   return (
-    <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
-      <div className='grid gap-8 row-gap-5 mb-8 lg:grid-cols-3 lg:row-gap-8'>
-        <h1>Shop Component</h1>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-8 row-gap-5 mb-8 lg:grid-cols-3 lg:row-gap-8">
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
