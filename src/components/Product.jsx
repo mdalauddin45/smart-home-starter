2f1897c94dd3da21410b5f154b096858c5b87d2c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-09 10:55:54 +0600

    dfgdf

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Product = () => {
-  const product = {}
-  const { name, picture, price, category } = product
+const Product = ({ product }) => {
+  const { name, picture, price, category } = product;
   return (
-    <div className='bg-gray-100 p-6 rounded shadow-lg'>
+    <div className="bg-gray-100 p-6 rounded shadow-lg">
       <img
-        className='object-cover w-full h-56 mb-6 rounded shadow-lg md:h-64 xl:h-80'
+        className="object-cover w-full h-56 mb-6 rounded shadow-lg md:h-64 xl:h-80"
         src={picture}
-        alt=''
+        alt=""
       />
-      <p className='mb-2 text-xl font-bold leading-none sm:text-2xl'>{name}</p>
-      <p className='text-gray-700 '>Category: {category}</p>
-      <p className='text-gray-700 font-bold'>Price: {price}$</p>
+      <p className="mb-2 text-xl font-bold leading-none sm:text-2xl">{name}</p>
+      <p className="text-gray-700 ">Category: {category}</p>
+      <p className="text-gray-700 font-bold">Price: {price}$</p>
       <button
-        type='button'
-        className='px-8 block w-full mt-4 py-3 font-semibold rounded-full bg-cyan-200 text-gray-800 hover:bg-cyan-400'
+        type="button"
+        className="px-8 block w-full mt-4 py-3 font-semibold rounded-full bg-cyan-200 text-gray-800 hover:bg-cyan-400"
       >
         Add To Cart
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
