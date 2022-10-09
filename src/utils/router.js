67edf467c33924a91f98fdef549b3baa3fc58c17
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-09 10:30:38 +0600

    a

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../components/ErrorPage";
+import Home from "../components/Home";
 import Main from "../components/Main";
 
 const router = createBrowserRouter([
@@ -7,6 +8,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Main></Main>,
     errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "/shop", element: <Home></Home> },
+      { path: "/cart", element: <Home></Home> },
+      { path: "/about", element: <Home></Home> },
+    ],
   },
 ]);
 
